145a36d90a381dbcd73f506602a07b97bc13d051
Jeremias Osuna <jeremias.o@geopagos.com>
2022-03-13 01:21:54 -0300

    Update and Delete method.

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,12 +6,19 @@ import { CategoriesController } from './controllers/categories.controller';
 import { OrdersController } from './controllers/orders.controller';
 import { UsersController } from './controllers/users.controller';
 import { CustomersController } from './controllers/customers.controller';
-import { BrandController } from './controllers/brand.controller';
 import { BrandsController } from './controllers/brands.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController, ProductsController, CategoriesController, OrdersController, UsersController, CustomersController, BrandController, BrandsController],
+  controllers: [
+    AppController,
+    ProductsController,
+    CategoriesController,
+    OrdersController,
+    UsersController,
+    CustomersController,
+    BrandsController,
+  ],
   providers: [AppService],
 })
 export class AppModule {}
--- src/controllers/products.controller.ts
+++ src/controllers/products.controller.ts
@@ -1,25 +1,65 @@
-import { Controller, Get, Param, Query } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+  Query,
+} from '@nestjs/common';
 
 @Controller('products')
 export class ProductsController {
   @Get('/')
-  getProducts(
+  getAll(
     @Query('limit') limit = 100,
     @Query('offset') offset = 0,
     @Query('brand') brand = 'track',
   ) {
     // const { limit, offset } = params;
 
-    return `products ${limit} ${offset} ${brand}`;
+    return {
+      limit: limit,
+      offset: offset,
+      brand: brand,
+    };
   }
 
   @Get('filter')
-  getProductFilter() {
-    return `Tengo un filtro`;
+  getFilter() {
+    return {
+      message: 'Filtro',
+    };
   }
 
   @Get(':id')
-  getProduct(@Param('id') id: number) {
-    return `product ${id}`;
+  getOne(@Param('id') id: number) {
+    return {
+      message: `Product ${id}`,
+    };
+  }
+
+  @Post()
+  create(@Body() payload: any) {
+    return {
+      message: 'Accion de crear',
+      payload,
+    };
+  }
+
+  @Put(':id')
+  update(@Param('id') id: number, @Body() payload: any) {
+    return {
+      id,
+      payload,
+    };
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: number) {
+    return {
+      message: `Se eliminó el producto ${id}`,
+    };
   }
 }
